115aed1f8105db57c32eca56033af5e484df8136
mateog147 <mateog147@hotmail.com>
2022-07-20 13:52:19 -0500

    se agrega modelo de ronda

--- bakend/marvel/domain/model/src/main/java/com/sofkau/model/ronda/Ronda.java
+++ bakend/marvel/domain/model/src/main/java/com/sofkau/model/ronda/Ronda.java
@@ -1,8 +1,20 @@
 package com.sofkau.model.ronda;
+import com.sofkau.model.carta.Carta;
+import com.sofkau.model.jugador.Jugador;
 import lombok.Builder;
 import lombok.Data;
+
+import java.util.Map;
 
 @Data
 @Builder(toBuilder = true)
 public class Ronda {
+
+    private String id;
+    Map<Carta, String> apuestas;
+
+    /*public void agregarApuesta(Carta carta, Jugador jugador){
+        this.apuestas.put(carta, jugador.id());
+    }*/
+
 }

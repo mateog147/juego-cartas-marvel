6b25e067fdd2051db13d0d9649fdf410e06974a5
juanir33 <guaca33@gmail.com>
2022-07-22 01:32:11 -0300

    Se agregan authservice y authguard, se configura firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase :{
+    apiKey: "AIzaSyDHgUVpSiMhc6LjujfjC5NhiCi07vPqmnU",
+    authDomain: "sofkau-heroes.firebaseapp.com",
+    projectId: "sofkau-heroes",
+    storageBucket: "sofkau-heroes.appspot.com",
+    messagingSenderId: "162611560419",
+    appId: "1:162611560419:web:88724e642e2b9c838eead6"
+  }
 };
 
 /*

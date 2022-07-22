c5090e2a4d7ea1595b335316cd7117b20acf8867
juanir33 <guaca33@gmail.com>
2022-07-22 14:38:18 -0300

    Se termina drag and drop, con recoleccion de datos

--- src/app/components/tablero/tablero.component.ts
+++ src/app/components/tablero/tablero.component.ts
@@ -11,20 +11,29 @@ export class TableroComponent implements OnInit {
   apuestas: Card[] = [{nombre: 'Tribunal Viviente', image: '../../../assets/maxresdefault.jpg', description:'El mejor de todos', xp: 99999999}];
   mazo: Card[] = [{nombre: 'Tribunal Viviente', image: '../../../assets/maxresdefault.jpg', description:'El mejor de todos', xp: 99999999},{nombre: 'Tribunal Viviente', image: '../../../assets/maxresdefault.jpg', description:'El mejor de todos', xp: 99999999}]
   constructor() { }
-
+ 
+ console = console;
+  
   ngOnInit(): void {
   }
   drop(event: CdkDragDrop<Card[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
         event.currentIndex,
-      );
+        
+      );this.newArray()
     }
   }
+  newArray(){
+    localStorage.setItem('mazo', JSON.stringify(this.mazo));
+    localStorage.setItem('apuesta', JSON.stringify(this.apuestas));
 
+  }
+ 
 }

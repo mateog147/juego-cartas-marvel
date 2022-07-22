239ffd0563f8a5c609b88b839760ede5932993b7
juanir33 <guaca33@gmail.com>
2022-07-22 01:40:04 -0300

    Se agregan card, avatar jugador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { AuthService } from './shared/services/auth.service';
+import { RoutingModule } from './routing/routing.module';
+import { CardComponent } from './components/card/card.component';
+import { TableroComponent } from './components/tablero/tablero.component';
+import { AvatarjugadorComponent } from './components/avatarjugador/avatarjugador.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
+    CardComponent,
+    TableroComponent,
+    AvatarjugadorComponent,
   ],
   imports: [
-    BrowserModule
+    AngularFireModule.initializeApp(environment.firebase, 'sofkau-heroes'),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
+    BrowserModule,
+    RoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AuthService],
+  bootstrap: [AppComponent],
+  exports: [
+    CardComponent
+  ],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/avatarjugador/avatarjugador.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-avatarjugador',
+  templateUrl: './avatarjugador.component.html',
+  styleUrls: ['./avatarjugador.component.css']
+})
+export class AvatarjugadorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

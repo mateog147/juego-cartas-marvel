5bc813685c286c011844632306f08dfa8f571a6a
juanir33 <guaca33@gmail.com>
2022-07-23 00:15:52 -0300

    Agregando condicional para apuestas

--- src/app/components/tablero/tablero.component.ts
+++ src/app/components/tablero/tablero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import {Card} from '../card/card.component'
 import { AuthService } from 'src/app/shared/services/auth.service';
@@ -10,19 +10,23 @@ import { CartaserviceService } from 'src/app/shared/services/cartaservice.servic
   templateUrl: './tablero.component.html',
   styleUrls: ['./tablero.component.css']
 })
-export class TableroComponent implements OnInit {
-  tablero: {status:boolean} = {status : true}
+export class TableroComponent implements OnInit , DoCheck {
+  tablero: {status:boolean} = {status : false}
   apuestas: Card[] = [];
   mazo: Card[] = [];
   constructor(public authService: AuthService, private jugador: JugadorserviceService, private carta : CartaserviceService) {}
+  ngDoCheck(): void {
+    if(this.apuestas.length === 4){
+      this.tablero.status = true;
+    }
+  }
  
  console = console;
 
-  
-  
  
   
   ngOnInit(): void {
+    this.jugador.nuevoJugador()
     this.carta.getAll().subscribe(data=>{
       data.forEach((item: Card) =>this.mazo.push(item));
     })

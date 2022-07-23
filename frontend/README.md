eb244ae36158d4c22ff30a3ef74218f0200c425d
mateog147 <mateog147@hotmail.com>
2022-07-22 19:38:56 -0500

    Frontend actualizado desde rama juan

--- frontend/README.md
+++ frontend/README.md
@@ -1,4 +1,4 @@
-# AngularFirebaseAuth
+# MarvelSofka
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.6.
 
--- frontend/src/app/components/tablero/tablero.component.ts
+++ frontend/src/app/components/tablero/tablero.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import {Card} from '../card/card.component'
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { User } from 'src/app/shared/services/user';
 @Component({
   selector: 'app-tablero',
   templateUrl: './tablero.component.html',
   styleUrls: ['./tablero.component.css']
 })
 export class TableroComponent implements OnInit {
-  tablero: {status:boolean} = {status : false}
-  constructor() { }
-
+  tablero: {status:boolean} = {status : true}
+  apuestas: Card[] = [{nombre: 'Viviente', image: '../../../assets/maxresdefault.jpg', description:'El mejor de todos', xp: 99999999},{nombre: 'Tribunal Viviente', image: '../../../assets/maxresdefault.jpg', description:'El mejor de todos', xp: 99999999},{nombre: 'Tribunal Viviente', image: '../../../assets/maxresdefault.jpg', description:'El mejor de todos', xp: 99999999}];
+  mazo: Card[] = [{nombre: 'Viviente', image: '../../../assets/maxresdefault.jpg', description:'El mejor de todos', xp: 99999999},{nombre: 'Tribunal Viviente', image: '../../../assets/maxresdefault.jpg', description:'El mejor de todos', xp: 99999999},{nombre: 'Tribunal Viviente', image: '../../../assets/maxresdefault.jpg', description:'El mejor de todos', xp: 99999999}]
+  constructor(public authService: AuthService) {}
+ 
+ console = console;
+  
   ngOnInit(): void {
   }
+  drop(event: CdkDragDrop<Card[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+        
+      );this.newArray()
+    }
+  }
+  newArray(){
+    localStorage.setItem('mazo', JSON.stringify(this.mazo));
+    localStorage.setItem('apuesta', JSON.stringify(this.apuestas));
 
+  }
+ 
 }

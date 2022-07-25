5c98806a2de74cd27b6b41a7c8b76f0b2b079ec4
mateog147 <mateog147@hotmail.com>
2022-07-24 22:34:05 -0500

    se agrega ultimo ganador y numero a la ronda

--- bakend/marvel/domain/usecase/src/main/java/com/sofkau/usecase/partida/gestionarganador/GestionarGanadorUseCase.java
+++ bakend/marvel/domain/usecase/src/main/java/com/sofkau/usecase/partida/gestionarganador/GestionarGanadorUseCase.java
@@ -9,13 +9,14 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class GestionarGanadorUseCase {
     private final GanadorRondaUseCase ganadorRondaUseCase;
-
+    private String nombreGanador;
     public Mono<Partida> gestionarGanador(Partida partida){
         String ganadorId = partida.getRonda().determinarGanador();
         System.out.println("GANO:" + ganadorId);
         return  Flux.fromIterable(partida.getJugadores())
                                     .map(jugador -> {
                                         if(jugador.id().equals(ganadorId)){
+                                            nombreGanador = jugador.getNombre();
                                             jugador.agregarCartas(partida.getRonda().entregarCartas());
                                         }
                                         return jugador;
@@ -28,7 +29,7 @@ public class GestionarGanadorUseCase {
                                         return partida;
                                     })
                                     .flatMap(partida1 -> {
-                                        return ganadorRondaUseCase.terminarRonda(partida1.getRonda());
+                                        return ganadorRondaUseCase.terminarRonda(partida1.getRonda(), nombreGanador);
                                     })
                                     .map(ronda -> partida.toBuilder().ronda(ronda).build());
 

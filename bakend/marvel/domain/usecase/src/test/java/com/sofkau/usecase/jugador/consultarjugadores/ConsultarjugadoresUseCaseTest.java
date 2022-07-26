140522be5f447de0a722f65a1937a714675b679d
mateog147 <mateog147@hotmail.com>
2022-07-25 22:05:16 -0500

    intento test no completo

--- bakend/marvel/domain/usecase/src/test/java/com/sofkau/usecase/jugador/consultarjugadores/ConsultarjugadoresUseCaseTest.java
+++ bakend/marvel/domain/usecase/src/test/java/com/sofkau/usecase/jugador/consultarjugadores/ConsultarjugadoresUseCaseTest.java
@@ -1,17 +1,16 @@
 package com.sofkau.usecase.jugador.consultarjugadores;
 
+
+
 import com.sofkau.model.jugador.Jugador;
 import com.sofkau.model.jugador.gateways.JugadorRepository;
-import org.assertj.core.util.Arrays;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
@@ -19,29 +18,62 @@ import reactor.test.StepVerifier;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static reactor.core.publisher.Mono.when;
 
+
+@ExtendWith(MockitoExtension.class)
+
+@SpringBootTest(classes = ConsultarjugadoresUseCaseTest.class)
 class ConsultarjugadoresUseCaseTest {
+
+    static class Invento implements JugadorRepository{
+
+        @Override
+        public Mono<Jugador> save(Jugador jugador) {
+            return null;
+        }
+
+        @Override
+        public Flux<Jugador> findAll() {
+            return Flux.just(Jugador.builder().id("1").build());
+        }
+
+        @Override
+        public Mono<Jugador> rendirseEnRonda(String jugadorId, Jugador jugador) {
+            return null;
+        }
+
+        @Override
+        public Mono<Jugador> findById(String id) {
+            return null;
+        }
+
+        @Override
+        public Flux<Jugador> findAllById(Iterable<String> ids) {
+            return null;
+        }
+
+        @Override
+        public Mono<Jugador> findByUid(String uid) {
+            return null;
+        }
+    }
+
+    JugadorRepository repository = new Invento();
 
     @Autowired
     ConsultarjugadoresUseCase useCase;
 
-    /*@Mock
-    private final JugadorRepository jugadorRepository;
-
-    @BeforeAll
-    public void setUp(){
-        MockitoAnnotations.openMocks(this);
-    }
     @Test
     public void consultar_un_jugador(){
-        //List<Jugador> jugadores = Arrays.asList(new Jugador("1","uid","test1",10,null));
-        Jugador jugador = new Jugador("1","uid","test1",10,null);
-        when(jugadorRepository.findAll()).thenReturn(Flux.just(jugador));
+        //List<Jugador> jugadores = Arrays.asList(Jugador.builder().id());
+
+        //when(repository.findAll()).thenReturn(Flux.just(Jugador.builder().id("1").build()));
+
 
         StepVerifier.create(useCase.consultarJugadores())
-                .expectNextMatches(jugador1 -> jugador1.id().equals("1"))
-                .verifyComplete();
+                .expectNextMatches(jugador -> jugador.id() == "1")
+                .thenCancel()
+                .verify();
 
-    }*/
+    }
 }
